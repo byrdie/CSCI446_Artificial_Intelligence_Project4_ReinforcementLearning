7325de571962e44c0ae7a737dfafc0115ea412f9
byrdie <roytsmart@gmail.com>
2016-12-06 21:07:14 -0700

    Added netbeans project

---
+++ src/CSCI446_Project4_ReinforcementLearning/nbproject/Makefile-variables.mk
@@ -0,0 +1,35 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+CND_BUILDDIR=build
+CND_DISTDIR=dist
+# Debug configuration
+CND_PLATFORM_Debug=GNU-Linux
+CND_ARTIFACT_DIR_Debug=dist/Debug/GNU-Linux
+CND_ARTIFACT_NAME_Debug=CSCI446_Project4_ReinforcementLearning
+CND_ARTIFACT_PATH_Debug=dist/Debug/GNU-Linux/CSCI446_Project4_ReinforcementLearning
+CND_PACKAGE_DIR_Debug=dist/Debug/GNU-Linux/package
+CND_PACKAGE_NAME_Debug=CSCI446Project4ReinforcementLearning.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/GNU-Linux/package/CSCI446Project4ReinforcementLearning.tar
+# Release configuration
+CND_PLATFORM_Release=GNU-Linux
+CND_ARTIFACT_DIR_Release=dist/Release/GNU-Linux
+CND_ARTIFACT_NAME_Release=CSCI446_Project4_ReinforcementLearning
+CND_ARTIFACT_PATH_Release=dist/Release/GNU-Linux/CSCI446_Project4_ReinforcementLearning
+CND_PACKAGE_DIR_Release=dist/Release/GNU-Linux/package
+CND_PACKAGE_NAME_Release=CSCI446Project4ReinforcementLearning.tar
+CND_PACKAGE_PATH_Release=dist/Release/GNU-Linux/package/CSCI446Project4ReinforcementLearning.tar
+#
+# include compiler specific variables
+#
+# dmake command
+ROOT:sh = test -f nbproject/private/Makefile-variables.mk || \
+	(mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk)
+#
+# gmake command
+.PHONY: $(shell test -f nbproject/private/Makefile-variables.mk || (mkdir -p nbproject/private && touch nbproject/private/Makefile-variables.mk))
+#
+include nbproject/private/Makefile-variables.mk
